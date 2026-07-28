aff68193aa800ab72bceccd8d29b1092a3aac5db
ExpectoPotatonum <hoojy1562002@gmail.com>
2026-07-29 00:33:33 +0800

    update cache version to v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'training-camp-v3';
+const CACHE_NAME = 'training-camp-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
